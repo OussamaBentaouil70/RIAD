b70c270b25320ac375e187d7a3da760a1075455e
OussamaBentaouil70 <ousamabentaouil1908@gmail.com>
2026-09-02 15:28:14 +0100

    change the whatsapp number

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = '34600000000';
+export const WHATSAPP_NUMBER = '34678798513';
 
 export function waLink(message?: string) {
   const base = `https://wa.me/${WHATSAPP_NUMBER}`;
